fc4a90bd295fbeda5a5c6c9545f424bdf5a27beb
Yago <yagos-s@live.com>
2022-10-25 15:43:51 -0300

    transformar array em string e string e array

---
+++ php/strings/telefones.php
@@ -0,0 +1,6 @@
+<?php
+
+$telefones = ['(11) 99999-9999', '(11) 99999-2222', '(11) 99999-3333', '(11) 99999-1111'];
+// O implode converte um array em strings e separa de acordo com uma substring que vc escolher
+echo implode(PHP_EOL,$telefones) . PHP_EOL;
+ 
--- php/strings/usuario.php
+++ php/strings/usuario.php
@@ -1,5 +1,6 @@
 <?php
 
+$nome = 'Vínicius Dias';
 $email = 'vinicius@alura.com.br';
 $senha = '123';
 //strpos consulta a posução da substring recebendo a variavel e a substring e
@@ -8,11 +9,20 @@ $posiçãoDoArroba = strpos($email,'@');
 $usuario = substr($email,0,$posiçãoDoArroba);
 
 
-echo substr($email, $posiçãoDoArroba+1) . PHP_EOL;
+//echo substr($email, $posiçãoDoArroba+1) . PHP_EOL;
 //consulta o tamanho da string em bytes
 
-echo mb_strtoupper($usuario) . PHP_EOL;
+//echo mb_strtoupper($usuario) . PHP_EOL;
+
+//mb_strlen conta a quantidade de caracteres
+
+//aplicando p list de forma abreviada no lugar de list($nome, $sobreNome)
+//o comando explode separa uma sitring a partir do valor definido (transforma item em array) e voccê pode definir um limite de array
+[$nome, $sobreNome] = explode(' ', $nome);
+
+//echo "Nome: $nome \n";
+//echo "Sobrenome: $sobreNome \n";
 
 
-
-
+$csv = 'Vínicius Dias,24,vinicius@alura.com.br';
+var_dump(explode(',',$csv));
