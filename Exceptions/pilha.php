21d20aa0397f964d19e30d3a588e8d1c6beb55e1
Yago <yagos-s@live.com>
2022-11-21 11:49:15 -0300

    Problemas e exceçoes, 'try - catch, e multi-catch para tratar mais de uma exceção por vez

--- Exceptions/pilha.php
+++ Exceptions/pilha.php
@@ -3,13 +3,26 @@
 function funcao1()
 {
     echo 'Entrei na função 1' . PHP_EOL;
-    funcao2();
+    try {
+        funcao2();
+    } catch (RuntimeException | DivisionByZeroError $erroOuExecao) {
+        echo $erroOuExecao->getMessage().PHP_EOL;
+        echo $erroOuExecao->getLine().PHP_EOL;
+        echo $erroOuExecao->getTraceAsString();
+        echo "Na função 1, eu resolvi o problema da função 2" . PHP_EOL;
+    }
     echo 'Saindo da função 1' . PHP_EOL;
 }
 
 function funcao2()
 {
+
     echo 'Entrei na função 2' . PHP_EOL;
+
+    $arrayFixo = new SplFixedArray(2);
+    $arrayFixo[3] = 'Valor';
+    intdiv(5,0);
+
     for ($i = 1; $i <= 5; $i++) {
         echo $i . PHP_EOL;
     }
